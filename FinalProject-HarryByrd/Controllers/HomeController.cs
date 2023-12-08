c8b4dbcd0cd61b2128a6e417f0d3affce4a4d519
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2023-12-08 14:20:46 -0600

    Add project files.

---
+++ FinalProject-HarryByrd/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+using FinalProject_HarryByrd.Models;
+
+namespace GameList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GameContext context { get; set; }
+
+        public HomeController(GameContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var games = context.Games.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(games);
+        }
+    }
+}
